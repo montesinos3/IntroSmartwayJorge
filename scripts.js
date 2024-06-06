6ea4ea54a853a902314d305ca10cd8c0b129fe11
montesinos3 <jormonbra@gmail.com>
2024-06-06 15:28:57 +0200

    modo oscuro persistente

--- scripts.js
+++ scripts.js
@@ -1,54 +1,43 @@
-if(sessionStorage.getItem('popup')!="no"){
-    swal({
-        title: "Bienvenido",
-        text: "Esta es la web de ByteStorm Solutions", 
-        icon:"info" ,
-        buttons: {
-            seguir:"Gracias",
-            noMostrar: {text: "No volver a mostrar", value: false}, 
-        },
-    }).then((value)=>{
-        if(value==false){
-            //document.cookie="popup=no; max-age=60"
-            sessionStorage.setItem('popup',"no")
-        }
-    });
+document.addEventListener("DOMContentLoaded", ready);
+
+
+let infModo=sessionStorage.getItem('modo');
+if(infModo==null) sessionStorage.setItem('modo', "claro");
+
+
+function ready() {
+    if(sessionStorage.getItem('popup')!="no"){
+        swal({
+            title: "Bienvenido",
+            text: "Esta es la web de ByteStorm Solutions", 
+            icon:"info" ,
+            buttons: {
+                seguir:"Gracias",
+                noMostrar: {text: "No volver a mostrar", value: false}, 
+            },
+        }).then((value)=>{
+            if(value==false){
+                //document.cookie="popup=no; max-age=60"
+                sessionStorage.setItem('popup',"no")
+            }
+        });
+    }
+    
+    let body = document.getElementsByTagName('body')[0];
+    let m = document.getElementsByTagName('nav')[0];
+    let f = document.getElementsByTagName('footer')[0];
+
+    if(sessionStorage.getItem('modo')=="claro"){
+        body.className="claro";
+        m.className="menuClaro";   
+        f.className="footerclaro";
+    } else{
+        body.className="oscuro";
+        m.className="menuOscuro";
+        f.className="footeroscuro";
+    }
 }
 
-let infoBody=sessionStorage.getItem('body');
-if(infoBody==null) sessionStorage.setItem('body', "claro");
-
-let body = document.getElementsByTagName('body')[0];
-if(sessionStorage.getItem('body')=="claro"){
-    body.className="claro";
-} else{
-    body.className="oscuro";
-}
-
-
-
-let infMenu=sessionStorage.getItem('menu');
-//alert(infoMenu)
-if(infMenu==null) sessionStorage.setItem('menu', "claro");
-//alert(sessionStorage.getItem('menu'))
-let m = document.getElementsByTagName('menu')[0];
-if(sessionStorage.getItem('menu')=="claro"){
-    m.className="menuClaro";
-} else{
-    m.className="menuOscuro";
-}
-
-
-
-let infFooter=sessionStorage.getItem('footer');
-if(infFooter==null) sessionStorage.setItem('footer', "claro");
-
-let f = document.getElementsByTagName('footer')[0];
-if(sessionStorage.getItem('footer')=="claro"){
-    f.className="footerclaro";
-} else{
-    f.className="footeroscuro";
-}
 
 
 
@@ -68,39 +57,22 @@ if(sessionStorage.getItem('footer')=="claro"){
 //  }
 
  function cambiarModo(){
-    let infoPag=sessionStorage.getItem('body');
-    let infoMenu=sessionStorage.getItem('menu');
-    let infoFooter=sessionStorage.getItem('footer');
-
+    let infoModo=sessionStorage.getItem('modo');
+    
     let pag=document.getElementsByTagName('body')[0];
-    if(infoPag=="claro"){
+    let footer= document.getElementsByTagName('footer')[0];
+    let menu=document.getElementsByTagName('nav')[0];
+    if(infoModo=="claro"){
         pag.className="oscuro";
-        sessionStorage.setItem('body', "oscuro");
+        footer.className="footeroscuro";
+        menu.className="menuOscuro";
+        sessionStorage.setItem('modo', "oscuro");
     } else{
         pag.className="claro";
-        sessionStorage.setItem('body', "claro");
-    } 
-    // alert(`El id del body es ${document.getElementsByTagName('body')[0].id}`);
-
-
-    let footer= document.getElementsByTagName('footer')[0];
-    if(infoFooter=="claro"){
-        footer.className="footeroscuro";
-        sessionStorage.setItem('footer', "oscuro");
-    } else{
         footer.className="footerclaro";
-        sessionStorage.setItem('footer', "claro");
-    } 
-    // alert(`La clase del footer es ${document.getElementsByTagName('footer')[0].className}`);
-
-    let menu=document.getElementsByTagName('nav')[0];
-    if(infoMenu=="claro"){
-        menu.className="menuOscuro";
-        sessionStorage.setItem('menu', "oscuro");
-    } else{
         menu.className="menuClaro";
-        sessionStorage.setItem('menu', "claro");
+        sessionStorage.setItem('modo', "claro");
     } 
-    // alert(`El id del menu es ${document.getElementsByTagName('nav')[0].id}`)
-   
 }
+
+
