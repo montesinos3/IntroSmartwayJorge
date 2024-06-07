e6558d9dce2eb9988a8d93b2897e9967392cc9f7
montesinos3 <jormonbra@gmail.com>
2024-06-07 16:03:18 +0200

    .

--- scripts.js
+++ scripts.js
@@ -38,25 +38,6 @@ if(infModo==null) sessionStorage.setItem('modo', "claro");
     }
 // }
 
-// ready();
-
-
-
-//  function cambiarModo(){
-//      let pag=document.getElementsByTagName('body')[0];
-//      (pag.className=="claro") ? pag.className="oscuro" : pag.className="claro";
-//      // alert(`El id del body es ${document.getElementsByTagName('body')[0].id}`);
-
-
-//      let footer= document.getElementsByTagName('footer')[0];
-//      (footer.className=="footerclaro") ? footer.className="footeroscuro" : footer.className="footerclaro";
-//      // alert(`La clase del footer es ${document.getElementsByTagName('footer')[0].className}`);
-
-//      let menu=document.getElementsByTagName('nav')[0];
-//      (menu.className=="menuClaro") ? menu.className="menuOscuro" : menu.className="menuClaro";
-//      // alert(`El id del menu es ${document.getElementsByTagName('nav')[0].id}`)
-//  }
-
  function cambiarModo(){
     let infoModo=sessionStorage.getItem('modo');
     
